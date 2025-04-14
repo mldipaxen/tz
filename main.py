a1b95fcdb6c7fa6928e76e6f66c982554183e2ff
mldipaxen <paxa@paxa.myguest.virtualbox.org>
2025-04-14 21:03:57 +0300

    final

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes.reserverout import router as resrouter
-from routes.tableroute import router as tablerouter
-
-
-app = FastAPI()
-app.include_router(resrouter)
-app.include_router(tablerouter)
-
